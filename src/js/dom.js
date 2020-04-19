f0b219c744f64d5ec2106416de4eafc1afa283bb
Charles Opute Odili <chaluwa@gmail.com>
2020-04-19 13:10:04 +0100

    add adapter for querySelectorAll
    Add function adapter for the document.querySelectorAll DOM function

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,6 +1,13 @@
-const select = (selector, parent) => {
+export const select = (selector, parent) => {
   if(parent) {
     return parent.querySelector(selector);
   }
   return document.querySelector(selector);
+};
+
+export const selectAll = (selector, parent) => {
+  if(parent) {
+    return parent.querySelectorAll(selector);
+  }
+  return document.querySelectorAll(selector);
 };
