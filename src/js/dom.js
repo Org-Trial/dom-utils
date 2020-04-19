0687504f8e66ea02750c7bd6acd983e76ad36c20
Charles Opute Odili <chaluwa@gmail.com>
2020-04-19 13:06:15 +0100

    address lint errors in codebase
    refactor code to clear up identified lint errors in the code

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,4 +1,6 @@
 const select = (selector, parent) => {
-  if(parent) return parent.querySelector(selector);
+  if(parent) {
+    return parent.querySelector(selector);
+  }
   return document.querySelector(selector);
 };
